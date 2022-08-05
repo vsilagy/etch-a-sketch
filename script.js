96e1fe0bc804e36ea79d66ffa955e11ba6229a40
Vlad Silagy <vladsilagy@gmail.com>
2022-08-05 01:23:30 -0400

    added control buttons, added grid size input range

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 const grid = document.getElementById("grid");
+const sliderSize = document.getElementById("slider-size");
+const sliderValue = document.getElementById("slider-value");
 // grid size
-let gridSize = 16;
 
 function renderGrid(size) {
+  // reset the grid
+  let squares = grid.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
+  // grid columns and rows
   grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  // grid squares
+  let gridSize = size * size;
+  for (let i = 0; i < gridSize; i++) {
+    const square = document.createElement("div");
+    square.classList.add = "grid-element";
+    grid.appendChild(square);
+  }
 }
 
-renderGrid(gridSize);
+renderGrid(16);
+
+function changeSize(value) {
+  renderGrid(value);
+  sliderSize.innerHTML = `${value} x ${value}`;
+}
